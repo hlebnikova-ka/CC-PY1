dad30bd368048bae229671a1e92d4d44507b423c
hlebnikova-ka <118115629+hlebnikova-ka@users.noreply.github.com>
2022-11-27 23:15:50 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -6,7 +6,7 @@ for key, value in enumerate(list_numbers):
         max_number = value
         max_element_index = key
 
-list_numbers[max_element_index] = list_numbers[-1];list_numbers[-1] = max_number
+list_numbers[max_element_index], list_numbers[-1] = list_numbers[-1], max_number
 
 print(list_numbers)
 # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
