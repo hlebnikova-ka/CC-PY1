d59dac9ae6fe3dd97170ee962bbce62a446cd05e
hlebnikova-ka <118115629+hlebnikova-ka@users.noreply.github.com>
2022-12-04 17:38:39 +0300

    Лабораторная работа 4

---
+++ task3.py
@@ -0,0 +1,13 @@
+def delete(list_, index=None):
+    if index is None:
+        list_ = list_[:-1]
+    else:
+        list_ = list_[0:index] + list_[index+1:]
+    return(list_)
+
+
+print(delete([0, 1, 2], index=0))  # [1, 2]
+print(delete([0, 1, 2], index=1))  # [0, 2]
+print(delete([0, 1, 2]))  # [0, 1]
+
+# Ответ:
